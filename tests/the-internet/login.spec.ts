73f896f6055055620fde81fd97f83fb7ea83bced
Hanh My Lien Mai <hanh.mylienmai@hds.com>
2026-03-04 16:41:59 +0700

    first commit

---
+++ tests/the-internet/login.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+//testcase = fx(Arrange, Action, Assert)
+
+//**
+// Open browser
+//Navigate to https://the-internet.herokuapp.com/login
+//Fill in username with tomsmith
+//Fill in the password with SuperSecretPassword!
+//Click on Login button
+//And the home page is appear
+ //*/
+
+test('test', async ({ page }) => {
+//arrange
+  await page.goto('https://the-internet.herokuapp.com/login');
+//action
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+//assertions
+  await page.getByRole('heading', { name: 'Welcome to the Secure Area.' }).click();
+  await page.getByText('You logged into a secure area').click();
+});
