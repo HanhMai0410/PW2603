2c892dfd6bc5c1d977acc710937844c12f2ce0e7
Hanh My Lien Mai <hanh.mylienmai@hitachids.com>
2026-03-27 11:23:07 +0700

    test push code git

--- tests/the-internet/broken-image.spec.ts
+++ tests/the-internet/broken-image.spec.ts
@@ -1,18 +1,18 @@
-import {test,expect} from '@playwright/test';
+// import {test,expect} from '@playwright/test';
 
-test('verify broken image', async ({page}) => {
-    await page.goto('/broken_images');
+// test('verify broken image', async ({page}) => {
+//     await page.goto('/broken_images');
     
-    const images = page.locator('img');
+//     const images = page.locator('img');
     
-    // Check if the image is present
-    const allImages = await images.all();
+//     // Check if the image is present
+//     const allImages = await images.all();
 
-    for (const image of allImages) {
-        const imgSrc = await image.getAttribute('src');
-        expect(imgSrc?.length).toBeGreaterThan(1)
-        const res = await page.request.get("https://the-internet.herokuapp.com/"+imgSrc)
-        console.log("Image src:", imgSrc);
-        expect(res.status()).toBe(200)// Ensure src attribute exists
-    }   
-});
+//     for (const image of allImages) {
+//         const imgSrc = await image.getAttribute('src');
+//         expect(imgSrc?.length).toBeGreaterThan(1)
+//         const res = await page.request.get("https://the-internet.herokuapp.com/"+imgSrc)
+//         console.log("Image src:", imgSrc);
+//         expect(res.status()).toBe(200)// Ensure src attribute exists
+//     }   
+// });
