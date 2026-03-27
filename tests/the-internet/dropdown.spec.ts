90e34e994da622afab9105cb2617363a1912e5fc
Hanh My Lien Mai <hanh.mylienmai@hitachids.com>
2026-03-27 11:08:48 +0700

    correct code at dropdown

--- tests/the-internet/dropdown.spec.ts
+++ tests/the-internet/dropdown.spec.ts
@@ -9,7 +9,6 @@ import { test, expect } from '@playwright/test';
 test('select single option from dropdown', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/dropdown');
 
-  await page.locator('#dropdown').selectOption('Option 1');
   await page.locator('#dropdown').selectOption({ label: 'Option 1' });
   await expect(page.locator('#dropdown')).toHaveValue('1');
   await expect(page.locator('#dropdown > option:checked')).toHaveText('Option 1');
